fa1203747db6588187386b717b09faeb05b806c4
ace <4yellowall@gmail.com>
2019-12-03 17:10:37 +0300

    almost working version

--- main.c
+++ main.c
@@ -9,87 +9,82 @@ void help() {
 }
 
 int isEnd(char c){
-    if (c == ' ')
+    if((c == ' ')||(c == '\n')||(c == '\t')||(c == '\r')||(c == '\0')||(c == EOF))
         return 1;
-    if((c == '\n')||(c == '\t')||(c == '\r')||(c == '\0'))
-        return 2;
     else
         return 0;
 }
 
-char *date(){
-    return "date";
-}
-char *time(){
-    return "time";
-}
-char *line(){
-    return "line";
-}
-char *counter(){
-    return "counter";
-}
 
-char *plusWord(char *word, int num){
+char* plusWord(char *word){//, int *numberOfSymbols){
     if (!strcmp(word, "%date%"))
-            return date();
+            return "date"; // date();
     else
-        if (!strcmp(word,"%time%"))
-            return time();
-        else
-            if (!strcmp(word, "%line%"))
-                return line();
-            else
-                if(!strcmp(word, "%counter%"))
-                    return counter();
-                else
-                {
-                   /* char *result;
-                    result = (char*)malloc(sizeof(char));
-                    result = (char*) realloc(result, num*sizeof(char));
-                    for (int j = 0; j < num; j++)
-                    {
-                        result[j] = word[j];
-                    }
-                    return result;*/
-                   return word;
-                }
-
+        if (!strcmp(word,"%time%")) {
+            //*numberOfSymbols = 4;
+            return "time";
+        }
+        else {
+            return word;
+        };
+    /*
+else
+if (!strcmp(word, "%line%"))
+    return line();
+else
+    if(!strcmp(word, "%counter%"))
+        return counter();*/
 }
 
 void readText(FILE *fileInput, FILE *fileOutput)
 {
-
-    int stdinFlag = 1;
-    while (stdinFlag == 1 && !feof(fileInput))
+    char *inputText = NULL;
+    inputText = (char*)malloc(sizeof(char));
+    int numberOfSymbolsInText = 0;
+    char currentSymbol = fgetc(fileInput);
+    while (currentSymbol != EOF)
     {
-        char *wordOnly = NULL;
-        wordOnly = (char*)malloc(sizeof(char));
-        int numberOfSymbolsInWord = 0;
-        char currentSymbol = fgetc(fileInput);
-
-        while (!feof(fileInput) && !isEnd(currentSymbol))
-        {
-            wordOnly = (char *) realloc(wordOnly, (numberOfSymbolsInWord + 1) * sizeof(char));
-            wordOnly[numberOfSymbolsInWord] = currentSymbol;
-            currentSymbol = fgetc(fileInput);
-            numberOfSymbolsInWord++;
-        }
-
-        wordOnly = (char *) realloc(wordOnly, sizeof(plusWord(wordOnly, numberOfSymbolsInWord)));
-        wordOnly = plusWord(wordOnly, numberOfSymbolsInWord);
-
-       for (int k = 0; k < strlen(wordOnly); k++)
-        {
-            fprintf(fileOutput,"%c", wordOnly[k]);
-        }//*/
-        //fprintf(fileOutput,"%s", wordOnly);
-        free(wordOnly);
-
-       //до контрл д
-        fprintf(fileOutput,"%c", currentSymbol);
+        inputText = (char *) realloc(inputText, (numberOfSymbolsInText + 1) * sizeof(char));
+        inputText[numberOfSymbolsInText] = currentSymbol;
+        currentSymbol = fgetc(fileInput);
+        numberOfSymbolsInText++;
     }
 
+    fprintf(fileOutput, "\n");
+
+    int k = 0;
+    while (k < numberOfSymbolsInText){
+        char *wordOnly = NULL;
+        wordOnly = (char *) malloc(sizeof(char));// */
+        int numberOfSymbolsInWord = 0;
+
+        while (k < numberOfSymbolsInText  && !isEnd(inputText[k])) {
+            wordOnly = (char *) realloc(wordOnly, (numberOfSymbolsInWord + 1) * sizeof(char));
+            wordOnly[numberOfSymbolsInWord] = inputText[k];
+            numberOfSymbolsInWord++;
+            k++;
+        }
+        fprintf(fileOutput, "%s", wordOnly);
+        if (!strcmp(wordOnly, "%time%"))
+        {
+            wordOnly = "time";
+            numberOfSymbolsInWord = 4;
+            fprintf(fileOutput, "\ntit\n");
+        }
+
+        //wordOnly = plusWord(wordOnly);//, &numberOfSymbolsInWord);
+        //numberOfSymbolsInWord = strlen(wordOnly);
+
+        for (int i = 0; i < numberOfSymbolsInWord; i++) {
+            fprintf(fileOutput, "%c", wordOnly[i]);
+        }//*/
+        fprintf(fileOutput, "%c", inputText[k]);
+        k++;
+        free(wordOnly);
+    }
+
+    free(inputText);
+// */
     return;
 };
 
